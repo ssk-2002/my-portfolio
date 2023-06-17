4275d918b5e0546892dd7620e50ec0b6780a0af7
ssk-2002 <salmanhere101@gmail.com>
2023-06-17 16:42:51 +0530

    CV and project desc updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -24,7 +24,7 @@ function Projects() {
               imgPath={smart}
               isBlog={false}
               title="Smart-Brain"
-              description="A React web-app that uses Clarifai API to detect faces in provided images (through URLs) and also displays the total entry count for specific users"
+              description="This is a React web application that utilizes the powerful Clarifai API to detect faces in images provided via URLs. With this app, you can simply enter the URL of an image, and it will accurately identify and highlight any faces present in the image. Additionally, the application keeps track of the total number of entries made by specific users, allowing you to easily view and monitor user activity. It's an intuitive and user-friendly solution for face detection and user entry tracking."
               ghLink="https://github.com/ssk-2002/smart-brain"
               demoLink="https://smartbrainapp-9ppj.onrender.com/"
             />
@@ -35,7 +35,7 @@ function Projects() {
               imgPath={sorting}
               isBlog={false}
               title="Sorting Visualizer"
-              description="A React Frontend Web App that displays the sorting process of 4 Sorting Agorithms with the help of animation "
+              description="This app vividly demonstrates the step-by-step sorting process of four popular sorting algorithms. You can witness the algorithms in action as they rearrange and organize data in real-time, providing a visual representation of how each algorithm functions. It's an interactive and educational tool that brings sorting algorithms to life, allowing you to explore and understand their inner workings with ease."
               ghLink="https://github.com/ssk-2002/sorting-visualizer"
               demoLink="https://ssk-2002.github.io/sorting-visualizer/"
             />
@@ -46,7 +46,7 @@ function Projects() {
               imgPath={huffman}
               isBlog={false}
               title="TXT Compressor"
-              description="This web application helps in compressing and de-compressing .txt files using Huffman Algorithm"
+              description="By leveraging Huffman coding techniques, the application intelligently reduces the file size while preserving the original data integrity. Whether you need to compress large text files for storage or transmit them more efficiently, this user-friendly tool provides a straightforward solution."
               ghLink="https://github.com/ssk-2002/txt-compressor-huffman"
               demoLink="https://ssk-2002.github.io/txt-compressor-huffman/"          
             />
@@ -57,7 +57,7 @@ function Projects() {
               imgPath={robo}
               isBlog={false}
               title="Robo-Friends"
-              description="A simple React web-app that fetches Random Robots (using hashes) from an API to render them and gives us a search bar to filter the result"
+              description="Imagine a Pokémon-like experience where you can explore a variety of unique robot characters. By fetching robots based on special hash values from an API, the app brings these delightful characters to your screen. You can enjoy the excitement of discovering different robots, each with their own special features and traits."
               ghLink="https://github.com/ssk-2002/robofriends"
               demoLink="https://ssk-2002.github.io/robofriends/"          
             />
